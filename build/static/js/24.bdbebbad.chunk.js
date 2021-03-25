9564547598d77331f886f44126a8d7479ea678e0
herry <herry@owner.com>
2021-03-25 16:32:58 +0530

    gitignore added env

---
+++ build/static/js/24.bdbebbad.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{566:function(e,n,t){"use strict";t.r(n);var a=t(97),c=t(98),r=t(101),i=t(99),o=t(100),u=t(1),s=t.n(u),d=function(e){function n(){return Object(a.a)(this,n),Object(r.a)(this,Object(i.a)(n).apply(this,arguments))}return Object(o.a)(n,e),Object(c.a)(n,[{key:"render",value:function(){return s.a.createElement("div",{className:"animated fadeIn"},"Hello world!")}}]),n}(u.Component);n.default=d}}]);
+//# sourceMappingURL=24.bdbebbad.chunk.js.map
